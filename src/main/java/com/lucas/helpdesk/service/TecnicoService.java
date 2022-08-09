ed1f2cdef87bb70907c02e6d4b699ef3217c2100
LucasKavetzki <lucaskavetzki@outlook.com>
2022-08-09 12:17:52 -0400

    exceção para CPF e Email já cadastrados

--- src/main/java/com/lucas/helpdesk/service/TecnicoService.java
+++ src/main/java/com/lucas/helpdesk/service/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucas.helpdesk.domain.Pessoa;
 import com.lucas.helpdesk.domain.Tecnico;
 import com.lucas.helpdesk.domain.dto.TecnicoDTO;
+import com.lucas.helpdesk.repositories.PessoaRepository;
 import com.lucas.helpdesk.repositories.TecnicoRepository;
+import com.lucas.helpdesk.service.exptions.DataIntegrityViolationException;
 import com.lucas.helpdesk.service.exptions.ObjectnotFouldException;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicoService {
 	
 	@Autowired
 	private TecnicoRepository repo;
+	@Autowired
+	private PessoaRepository repop;
+
 	
 	
 	
@@ -30,10 +36,25 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repo.save(newObj);
 	
 	}
 
+	//VALIDAR POR CPF
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = repop.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado!");
+		}
+		
+		//Validar EMAIL
+		obj = repop.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado!");
+	}
+
 	
+}
 }
